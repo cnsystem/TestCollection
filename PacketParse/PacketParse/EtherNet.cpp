292475ca6761a2800f024ac4b9da445d0cd41367
cnsystem <cnsystem@yeah.net>
2013-09-03 23:25:58 +0800

    https://github.com/cnsystem/TestCollection.git

---
+++ PacketParse/PacketParse/EtherNet.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "EtherNet.h"
+
+
+EtherNet::EtherNet(void)
+{
+}
+
+
+EtherNet::~EtherNet(void)
+{
+}
---
+++ PacketParse/PacketParse/EtherNet.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "protocol.h"
+class EtherNet :
+	public Protocol
+{
+public:
+	EtherNet(void);
+	~EtherNet(void);
+};
+
